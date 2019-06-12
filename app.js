25fa99b981b1fbfc6b01600c5675fc43b9162878
maugares <mauro.garciaesteban@gmail.com>
2019-06-12 20:16:37 +0200

    Require the express-graphql middleware

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
 const port = 4000;
+
+app.use('/graphql', graphqlHTTP({
+  
+}))
 
 app.listen(port, () => {
   console.log(`Listening for requests on port ${port}`)
