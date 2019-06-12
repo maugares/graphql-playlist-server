b2d58036d0ec4a2648dbb4b532e92f68b16ab534
maugares <mauro.garciaesteban@gmail.com>
2019-06-12 20:04:40 +0200

    Add nodemon

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-const port = 4000
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Listening for requests on port ${port}`)
-})
+});
