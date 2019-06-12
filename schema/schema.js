75bee73494d5b9c35e5d35a6575707c54c5373d2
maugares <mauro.garciaesteban@gmail.com>
2019-06-12 21:53:14 +0200

    Redefine id as type ID

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,25 @@
 const graphql = require('graphql');
 const _ = require('lodash') // Allows to search in arrays, objects and strings
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLSchema
+} = graphql;
 
 // Dummy data
 const books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: 1 },
-  { name: 'The Final Empire', genre: 'Fantasy', id: 2 },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: 3 },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
 // Define Book Type
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLInt },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
@@ -26,7 +31,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLInt } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // Code to get data from db / other source
         return _.find(books, { id: args.id });
