2aaecb3447313a5099d3665b09adacc414552f78
maugares <mauro.garciaesteban@gmail.com>
2019-06-12 21:23:24 +0200

    Require schema in app.js & include it in the middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema')
 
 const app = express();
 
 const port = 4000;
 
 app.use('/graphql', graphqlHTTP({
-
+  schema
 }));
 
 app.listen(port, () => {
