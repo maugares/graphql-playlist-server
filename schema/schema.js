0a095376a1dc612648d5d9af4de45e292a3e6116
maugares <mauro.garciaesteban@gmail.com>
2019-06-13 17:40:11 +0200

    Add authorId to books dummy data

--- schema/schema.js
+++ schema/schema.js
@@ -11,9 +11,9 @@ const {
 
 // Dummy data
 const books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 
 const authors = [
