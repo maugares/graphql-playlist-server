bbbec9f8bb55e4d8cb6e3dfce5cba9f11382ec2c
maugares <mauro.garciaesteban@gmail.com>
2019-06-18 23:59:02 +0200

    Create an addAuthor mutation method

--- schema/schema.js
+++ schema/schema.js
@@ -2,6 +2,8 @@
 import {
   GraphQLObjectType,
   GraphQLID,
+  GraphQLString,
+  GraphQLInt,
   GraphQLSchema,
   GraphQLList,
 } from 'graphql';
@@ -15,10 +17,13 @@ import BookType from './types/BookType'
 import Author from '../models/author'
 import Book from '../models/book'
 
-// Define Root Query
+// Define the Root Query - this is how data is Read (GET) - cRud
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
+  
+  // Define the methods for Reading the data stored in the database
   fields: {
+    // Returns a book with a given ID
     book: {
       type: BookType,
       args: { id: { type: GraphQLID } },
@@ -27,6 +32,7 @@ const RootQuery = new GraphQLObjectType({
         // return _.find(books, { id: args.id });
       }
     },
+    // Returns an author with a given ID
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
@@ -35,12 +41,14 @@ const RootQuery = new GraphQLObjectType({
         // return authors.find(author => author.id === args.id);
       }
     },
+    // Returns an array with all the books stored in the database
     books: {
       type: new GraphQLList(BookType),
       resolve() {
         // return books;
       }
     },
+    // Returns an array with all the authors stored in the database
     authors: {
       type: new GraphQLList(AuthorType),
       resolve() {
@@ -50,6 +58,33 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+// Define the Mutation Query - this is how data is Created (POST), Removed (DELETE), Updated (PUT) - CrUD
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  
+  // Define the methods for Mutating/Changing elements in the database
+  fields: {
+    // Add an author to the database
+    addAuthor: {
+      type: AuthorType,
+      // Data sent in the POST request
+      args: {
+        name: { type: GraphQLString},
+        age: { type: GraphQLInt},
+      },
+      resolve(parent, args){
+        let author = new Author ({
+          name: args.name,
+          age: args.age
+        });
+        // Save the new Author and Return the values when queried
+        return author.save();
+      }
+    },
+  }
+});
+
 export default new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation,
 });
